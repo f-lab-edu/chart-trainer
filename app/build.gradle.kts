ebbbcbf333b12fe6bd0405d172e9ab7e38339ae3
yessorae <yessorae@gmail.com>
2024-05-09 00:47:22 +0900

    [CT-1-1] 프로젝트 생성 후 compileSdk와 라이브러리 버전 호환 에러 해소

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.yessorae.chartrainer"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.yessorae.chartrainer"

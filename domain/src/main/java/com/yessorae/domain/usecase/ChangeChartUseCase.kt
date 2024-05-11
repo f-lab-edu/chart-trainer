bf8e0e252d37cf2f2c7692a7d020d22f3276a916
yessorae <yessorae@gmail.com>
2024-05-11 20:35:27 +0900

    [CT-4] 게임데이터 조회, 매매하기, 다음 턴 불러오기, 차트 변경 UseCase 추가

---
+++ domain/src/main/java/com/yessorae/domain/usecase/ChangeChartUseCase.kt
@@ -0,0 +1,27 @@
+package com.yessorae.domain.usecase
+
+import com.yessorae.domain.exception.ChartGameException
+import com.yessorae.domain.repository.ChartGameRepository
+import com.yessorae.domain.repository.ChartRepository
+import javax.inject.Inject
+
+class ChangeChartUseCase @Inject constructor(
+    private val chartRepository: ChartRepository,
+    private val chartGameRepository: ChartGameRepository
+) {
+    suspend operator fun invoke(gameId: Long) {
+        val oldChartGame = chartGameRepository.fetchChartGame(gameId = gameId)
+
+        if (oldChartGame.isGameEnd) {
+            throw ChartGameException.CanNotChangeChartException(
+                message = "can't change chart because game has been end"
+            )
+        }
+
+        chartGameRepository.updateChartGame(
+            chartGame = oldChartGame.createFromNewChart(
+                newChart = chartRepository.fetchNewChartRandomly()
+            )
+        )
+    }
+}
---
+++ domain/src/main/java/com/yessorae/domain/usecase/SubscribeChartGameUseCase.kt
@@ -0,0 +1,32 @@
+package com.yessorae.domain.usecase
+
+import com.yessorae.domain.entity.ChartGame
+import com.yessorae.domain.repository.ChartGameRepository
+import com.yessorae.domain.repository.ChartRepository
+import com.yessorae.domain.repository.UserRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+// TODO::NOW 새로 만드는 역할까지 하는 게 괜찮을지? 일단 기술과 관련없는 기획자/유저입장에서 이름짓고 로직 작성중
+class SubscribeChartGameUseCase @Inject constructor(
+    private val userRepository: UserRepository,
+    private val chartRepository: ChartRepository,
+    private val chartGameRepository: ChartGameRepository
+) {
+    suspend operator fun invoke(
+        gameId: Long?
+    ): Flow<ChartGame> {
+        if (gameId == null) {
+            val newGameId = chartGameRepository.saveChartGame(
+                chartGame = ChartGame.new(
+                    chart = chartRepository.fetchNewChartRandomly(),
+                    totalTurn = userRepository.fetchTotalTurnConfig(),
+                    startBalance = userRepository.fetchCurrentBalance()
+                )
+            )
+            return chartGameRepository.fetchChartFlowStream(gameId = newGameId)
+        }
+
+        return chartGameRepository.fetchChartFlowStream(gameId = gameId)
+    }
+}

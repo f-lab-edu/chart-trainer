60061962151282aa008c11fd76c5d74e919ba28b
yessorae <yessorae@gmail.com>
2024-05-11 20:32:49 +0900

    [CT-4] 커스텀 예외 추가

---
+++ domain/src/main/java/com/yessorae/domain/exception/ChartGameException.kt
@@ -0,0 +1,7 @@
+package com.yessorae.domain.exception
+
+sealed class ChartGameException(override val message: String = "") : Exception(message) {
+    data class CanNotChangeChartException(
+        override val message: String
+    ) : ChartGameException(message)
+}

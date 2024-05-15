6f95433c9049ab77d9a9f795a00d81003dd8c905
yessorae <yessorae@gmail.com>
2024-05-15 12:09:07 +0900

    [CT-4] 리뷰반영: suspend fun 기준에 맞게 수정

--- domain/src/main/java/com/yessorae/domain/repository/ChartGameRepository.kt
+++ domain/src/main/java/com/yessorae/domain/repository/ChartGameRepository.kt
@@ -4,7 +4,7 @@ import com.yessorae.domain.entity.ChartGame
 import kotlinx.coroutines.flow.Flow
 
 interface ChartGameRepository {
-    fun saveChartGame(chartGame: ChartGame): Long
+    suspend fun saveChartGame(chartGame: ChartGame): Long
 
     fun fetchChartFlowStream(gameId: Long): Flow<ChartGame>
 

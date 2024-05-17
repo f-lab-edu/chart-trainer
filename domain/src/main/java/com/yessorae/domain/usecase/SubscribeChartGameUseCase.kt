5395f0103a0c4b12c45047a5b29b2498ada21d10
yessorae <yessorae@gmail.com>
2024-05-17 21:31:33 +0900

    [CT-4] 리뷰 반영: 예외를 값으로 리턴 + 개선: UseCase를 Flow로 통일

--- domain/src/main/java/com/yessorae/domain/usecase/SubscribeChartGameUseCase.kt
+++ domain/src/main/java/com/yessorae/domain/usecase/SubscribeChartGameUseCase.kt
@@ -1,31 +1,36 @@
 package com.yessorae.domain.usecase
 
+import com.yessorae.domain.common.Result
+import com.yessorae.domain.common.delegateValueResultFlow
 import com.yessorae.domain.entity.ChartGame
 import com.yessorae.domain.repository.ChartGameRepository
 import com.yessorae.domain.repository.ChartRepository
 import com.yessorae.domain.repository.UserRepository
-import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
 class SubscribeChartGameUseCase @Inject constructor(
     private val userRepository: UserRepository,
     private val chartRepository: ChartRepository,
     private val chartGameRepository: ChartGameRepository
 ) {
-    suspend operator fun invoke(gameId: Long?): Flow<ChartGame> {
+    suspend operator fun invoke(gameId: Long?): Flow<Result<ChartGame>> {
         if (gameId == null) {
-            val newGameId =
-                chartGameRepository.saveChartGame(
-                    chartGame =
-                    ChartGame.new(
-                        chart = chartRepository.fetchNewChartRandomly(),
-                        totalTurn = userRepository.fetchTotalTurnConfig(),
-                        startBalance = userRepository.fetchCurrentBalance()
-                    )
+            val newGameId = chartGameRepository.saveChartGame(
+                chartGame = ChartGame.new(
+                    chart = chartRepository.fetchNewChartRandomly(),
+                    totalTurn = userRepository.fetchTotalTurnConfig(),
+                    startBalance = userRepository.fetchCurrentBalance()
                 )
-            return chartGameRepository.fetchChartFlowStream(gameId = newGameId)
+            )
+
+            return chartGameRepository
+                .fetchChartFlowStream(gameId = newGameId)
+                .delegateValueResultFlow()
         }
 
-        return chartGameRepository.fetchChartFlowStream(gameId = gameId)
+        return chartGameRepository
+            .fetchChartFlowStream(gameId = gameId)
+            .delegateValueResultFlow()
     }
 }

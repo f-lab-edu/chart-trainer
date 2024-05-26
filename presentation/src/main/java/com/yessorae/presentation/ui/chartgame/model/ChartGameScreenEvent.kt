47296a63ce5b619d7af28eb53ccf512716ab60d2
yessorae <yessorae@gmail.com>
2024-05-26 11:47:35 +0900

    [CT-2-1] 화면 모델 정의 (수정 및 추가)

--- presentation/src/main/java/com/yessorae/presentation/ui/chartgame/model/ChartGameScreenEvent.kt
+++
@@ -1,5 +0,0 @@
-package com.yessorae.presentation.ui.chartgame.model
-
-sealed interface ChartGameScreenEvent {
-    data class Toast(val text: String) : ChartGameScreenEvent
-}
--- presentation/src/main/java/com/yessorae/presentation/ui/chartgame/model/ChartGameUserAction.kt
+++ presentation/src/main/java/com/yessorae/presentation/ui/chartgame/model/ChartGameUserAction.kt
@@ -1,11 +1,39 @@
 package com.yessorae.presentation.ui.chartgame.model
 
-sealed interface ChartGameUserAction {
-    object ClickNewChartButton : ChartGameUserAction
-    object ClickChartGameHistoryButton : ChartGameUserAction
-    object ClickQuitGameButton : ChartGameUserAction
-    object ClickStartGameButton : ChartGameUserAction
-    object ClickBuyButton : ChartGameUserAction
-    object ClickSellButton : ChartGameUserAction
-    object ClickNextTickButton : ChartGameUserAction
+sealed interface ChartGameScreenUserAction {
+    object ClickNewChartButtonScreen : ChartGameScreenUserAction
+    object ClickChartGameScreenHistoryButton : ChartGameScreenUserAction
+    object ClickQuitGameButtonScreen : ChartGameScreenUserAction
+    object ClickBuyButton : ChartGameScreenUserAction
+    object ClickSellButton : ChartGameScreenUserAction
+    object ClickNextTickButton : ChartGameScreenUserAction
 }
+
+sealed interface BuyingOrderUiUserAction {
+    object ClickInput : BuyingOrderUiUserAction
+    object ClickSellButton : BuyingOrderUiUserAction
+    object ClickCancelButton : BuyingOrderUiUserAction
+    object DoSystemBack : BuyingOrderUiUserAction
+    data class ClickKeyPad(
+        val keyPad: TradeOrderKeyPad
+    ) : BuyingOrderUiUserAction
+
+    data class ClickPercentageShortCut(
+        val percent: Int
+    ) : BuyingOrderUiUserAction
+}
+
+sealed interface SellingOrderUiUserAction {
+    object ClickInput : SellingOrderUiUserAction
+    object ClickSellingButton : SellingOrderUiUserAction
+    object ClickCancelButton : SellingOrderUiUserAction
+    object DoSystemBack : SellingOrderUiUserAction
+    data class ClickPercentageShortCut(
+        val percent: Int
+    ) : SellingOrderUiUserAction
+
+    data class ClickKeyPad(
+        val keyPad: TradeOrderKeyPad
+    ) : SellingOrderUiUserAction
+}
+

98ce48091db4538eee4add9bce0c57087e184459
yessorae <yessorae@gmail.com>
2024-06-07 23:06:19 +0900

    [CT-5-2] UserRepository 구현

--- data/src/main/java/com/yessorae/data/repository/UserRepositoryImpl.kt
+++ data/src/main/java/com/yessorae/data/repository/UserRepositoryImpl.kt
@@ -1,23 +1,24 @@
 package com.yessorae.data.repository
 
+import com.yessorae.data.source.local.preference.ChartTrainerPreferencesDataSource
 import com.yessorae.domain.entity.User
 import com.yessorae.domain.repository.UserRepository
 import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
-class UserRepositoryImpl @Inject constructor() : UserRepository {
+class UserRepositoryImpl @Inject constructor(
+    private val chartTrainerPreferencesDataSource: ChartTrainerPreferencesDataSource
+) : UserRepository {
     override fun fetchUserAsFlow(): Flow<User> {
-        // TODO::LATER CT-5-2 에서 구현 필요, 기존 기능 실행은 되어야해서 임시조치
-        return flow { }
+        return chartTrainerPreferencesDataSource.userFlow
     }
 
     override suspend fun fetchUser(): User {
-        // TODO::LATER CT-5-2 에서 구현 필요, 기존 기능 실행은 되어야 해서 임시조치
-        return User.createInitialUser()
+        return chartTrainerPreferencesDataSource.getUser()
     }
 
     override suspend fun updateUser(user: User) {
-        TODO("Not yet implemented")
+        chartTrainerPreferencesDataSource.updateUser(user = user)
     }
 }

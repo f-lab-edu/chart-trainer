e6cb88d53be181791991516d805fd05b1d03b98d
yessorae <yessorae@gmail.com>
2024-06-07 22:38:37 +0900

    [CT-5-1] fix: 임시 런타임 에러 수정 (현재 브랜치 범위를 벗어나는 구현이라 임시 조치만)

--- data/src/main/java/com/yessorae/data/repository/UserRepositoryImpl.kt
+++ data/src/main/java/com/yessorae/data/repository/UserRepositoryImpl.kt
@@ -4,14 +4,17 @@ import com.yessorae.domain.entity.User
 import com.yessorae.domain.repository.UserRepository
 import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 class UserRepositoryImpl @Inject constructor() : UserRepository {
     override fun fetchUserAsFlow(): Flow<User> {
-        TODO("Not yet implemented")
+        // TODO::LATER CT-5-2 에서 구현 필요, 기존 기능 실행은 되어야해서 임시조치
+        return flow { }
     }
 
     override suspend fun fetchUser(): User {
-        TODO("Not yet implemented")
+        // TODO::LATER CT-5-2 에서 구현 필요, 기존 기능 실행은 되어야 해서 임시조치
+        return User.createInitialUser()
     }
 
     override suspend fun updateUser(user: User) {

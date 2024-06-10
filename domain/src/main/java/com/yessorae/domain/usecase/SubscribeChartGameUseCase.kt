6880cdf37d745c6114cf3af71198ec38747098de
yessorae <yessorae@gmail.com>
2024-06-11 00:59:07 +0900

    [CT-5-1] 리뷰반영 - SettingRepository 를 제거하고 UserRepository 로 통합

--- domain/src/main/java/com/yessorae/domain/usecase/SubscribeChartGameUseCase.kt
+++ domain/src/main/java/com/yessorae/domain/usecase/SubscribeChartGameUseCase.kt
@@ -5,20 +5,18 @@ import com.yessorae.domain.common.delegateValueResultFlow
 import com.yessorae.domain.entity.ChartGame
 import com.yessorae.domain.repository.ChartGameRepository
 import com.yessorae.domain.repository.ChartRepository
-import com.yessorae.domain.repository.SettingRepository
 import com.yessorae.domain.repository.UserRepository
 import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
 
 class SubscribeChartGameUseCase @Inject constructor(
     private val userRepository: UserRepository,
-    private val settingRepository: SettingRepository,
     private val chartRepository: ChartRepository,
     private val chartGameRepository: ChartGameRepository
 ) {
     suspend operator fun invoke(gameId: Long?): Flow<Result<ChartGame>> {
         if (gameId == null) {
-            val totalTurn = settingRepository.fetchTotalTurn()
+            val totalTurn = userRepository.fetchTotalTurn()
 
             val newGameId = chartGameRepository.createNewChartGame(
                 chartGame = ChartGame.new(

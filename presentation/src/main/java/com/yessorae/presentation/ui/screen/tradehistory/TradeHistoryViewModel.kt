8c779fa81873a0c0a700055ea17f8a04655a66a8
yessorae <yessorae@gmail.com>
2024-06-15 17:16:16 +0900

    [CT-6-1] 게임내역 화면의 ViewModel 추가

---
+++ presentation/src/main/java/com/yessorae/presentation/ui/screen/tradehistory/TradeHistoryViewModel.kt
@@ -0,0 +1,101 @@
+package com.yessorae.presentation.ui.screen.tradehistory
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.yessorae.domain.common.ChartTrainerLogger
+import com.yessorae.domain.common.Result
+import com.yessorae.domain.entity.trade.Trade
+import com.yessorae.domain.usecase.SubscribePagedTradeHistoryUseCase
+import com.yessorae.presentation.ui.screen.tradehistory.model.TradeHistoryScreenEvent
+import com.yessorae.presentation.ui.screen.tradehistory.model.TradeHistoryScreenModel
+import com.yessorae.presentation.ui.screen.tradehistory.model.TradeHistoryScreenUserAction
+import com.yessorae.presentation.ui.screen.tradehistory.model.asTradeHistoryListItemModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.onSubscription
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.plus
+
+@HiltViewModel
+class TradeHistoryViewModel @Inject constructor(
+    private val subscribePagedTradeHistoryUseCase: SubscribePagedTradeHistoryUseCase,
+    private val logger: ChartTrainerLogger,
+    savedStateHandle: SavedStateHandle
+) : ViewModel() {
+    // TODO::Refactoring
+    private val chartGameId: Long =
+        savedStateHandle["chartGameId"] ?: throw IllegalArgumentException("chartGameId is required")
+
+    private val _tradeHistoryScreen = MutableStateFlow(TradeHistoryScreenModel())
+    val tradeHistoryScreen: StateFlow<TradeHistoryScreenModel> =
+        _tradeHistoryScreen
+            .onSubscription {
+                subscribePagedTradeHistory()
+            }
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(5_000L),
+                initialValue = TradeHistoryScreenModel()
+            )
+
+    private val _screenEvent = MutableSharedFlow<TradeHistoryScreenEvent>()
+    val screenEvent: SharedFlow<TradeHistoryScreenEvent> = _screenEvent.asSharedFlow()
+
+    private val coroutineExceptionHandler = CoroutineExceptionHandler { _, throwable ->
+        handleFailure(throwable)
+    }
+
+    private val scope = viewModelScope + coroutineExceptionHandler
+
+    private fun subscribePagedTradeHistory() =
+        scope.launch {
+            subscribePagedTradeHistoryUseCase(gameId = chartGameId).collect { result ->
+                when (result) {
+                    is Result.Loading -> {
+                        _tradeHistoryScreen.update { old ->
+                            old.copy(loading = true)
+                        }
+                    }
+
+                    is Result.Success -> {
+                        _tradeHistoryScreen.value = TradeHistoryScreenModel(
+                            totalTurn = result.data.totalTurn,
+                            tradeHistories = result.data.trades.map(
+                                transform = Trade::asTradeHistoryListItemModel
+                            ),
+                            loading = false,
+                            none = result.data.trades.isEmpty()
+                        )
+                    }
+
+                    is Result.Failure -> {
+                        handleFailure(result.throwable)
+                    }
+                }
+            }
+        }
+
+    fun handleUserAction(userAction: TradeHistoryScreenUserAction) {
+        when (userAction) {
+            TradeHistoryScreenUserAction.ClickCloseButton -> {
+                _screenEvent.tryEmit(TradeHistoryScreenEvent.NavigateBack)
+            }
+        }
+    }
+
+    private fun handleFailure(throwable: Throwable) =
+        viewModelScope.launch {
+            _screenEvent.emit(TradeHistoryScreenEvent.UnknownError)
+            logger.cehLog(throwable)
+        }
+}

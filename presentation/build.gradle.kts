d8868f291acd40b795df64eb2c429a1155161c9c
yessorae <yessorae@gmail.com>
2024-06-17 20:39:44 +0900

    [CT-7] isCoreLibraryDesugaringEnabled

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -38,6 +38,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {

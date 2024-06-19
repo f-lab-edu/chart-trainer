44837cb1b75d4079fb8ddd097d02769b3dc978ff
yessorae <yessorae@gmail.com>
2024-06-19 19:55:19 +0900

    [CT-23] 리뷰반영: Q; 처음 한번만쓰이기 위해 필드를 선언하는게 좋을까요?

--- presentation/src/main/java/com/yessorae/presentation/ui/screen/chartgame/ChartGameViewModel.kt
+++ presentation/src/main/java/com/yessorae/presentation/ui/screen/chartgame/ChartGameViewModel.kt
@@ -46,11 +46,8 @@ class ChartGameViewModel @Inject constructor(
     private val updateNextTickUseCase: UpdateNextTickUseCase,
     private val quitChartGameUseCase: QuitChartGameUseCase,
     private val logger: ChartTrainerLogger,
-    savedStateHandle: SavedStateHandle
+    private val savedStateHandle: SavedStateHandle
 ) : ViewModel() {
-    private val gameIdFromPrevScreen: Long? =
-        savedStateHandle.get<String>(CHART_GAME_ID_ARG_KEY)?.toLongOrNull()
-
     private val _screenState = MutableStateFlow(ChartGameScreenState())
     val screenState: StateFlow<ChartGameScreenState> =
         _screenState
@@ -67,7 +64,9 @@ class ChartGameViewModel @Inject constructor(
 
     private fun subscribeChartGame() =
         viewModelScope.launch {
-            subscribeChartGameUseCase(gameId = gameIdFromPrevScreen).collectLatest { result ->
+            subscribeChartGameUseCase(
+                gameId = savedStateHandle.get<String>(CHART_GAME_ID_ARG_KEY)?.toLongOrNull()
+            ).collectLatest { result ->
                 when (result) {
                     is Result.Loading -> _screenState.update { old -> old.copy(showLoading = true) }
                     is Result.Success -> updateGameData(result.data)
